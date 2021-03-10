dd09e626e606ce6c506234dc842ce0449c0c6fa5
Quinn Do <quinn.d@webprovise.com>
2021-03-10 10:33:39 +0700

    package.json

---
+++ src/Card.tsx
@@ -0,0 +1,12 @@
+import { FunctionComponent } from 'react';
+
+interface OwnProps {}
+
+type Props = OwnProps;
+
+const Card: FunctionComponent<Props> = (props) => {
+
+  return (<div>d</div>);
+};
+
+export default Card;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+// tesididi
+export const Greeter = (name: string) => `Fuck didddddddddddddddddddddddddd ${name}`;
+export const Greeter2 = (name: string) => `Fuck aaa ${name}`;
+export const Greeter3 = (name: string) => `Hate ${name}`;
+export const Greeter4 = (name: string) => `Hate ddddddd ${name}`;
+export const Greeter5 = (name: string) => `Love 5 ${name}`;
